dcc01167635577622dc53c86bc5ca342be95a7bc
baijuka <baijuka@yahoo.com>
2021-06-27 13:05:48 +0000

    Retrieve last row of data from the stock wroksheet

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -60,7 +61,25 @@ def update_sales_worksheet(data):
     sales_worksheet.append_row(data)
     print("Sales worksheet updated successfully.\n")
 
-data = get_sales_data()
+def calculate_surplus_data(sales_row):
+    """
+    Compare sales with stock and calculate surplus for each item
+    Positive surplus indicates waste and negative surplus indicates 
+    extra made when stock was sold out
+    """
+    print("Calculating surplus data")
+    stock = SHEET.worksheet('stock').get_all_values()
+    stock_row = stock[-1]
+    print(stock_row)
 
-sales_data = [int(num) for num in data]
-update_sales_worksheet(sales_data)
+def main():
+    """
+    Call all functions
+    """
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+    calculate_surplus_data(sales_data)
+
+print("Welcome to Love Sandwhich Automation")
+main()
